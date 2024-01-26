2986e81d0440fb60562e185d71f4959850e80ca0
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-01-26 09:48:11 -0800

    Task 4

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -31,13 +31,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = saved_head;
 		return (-1);
 	}
-
 	if (0 == index)
 	{
+		tmp = (*head)->next;
 		free(*head);
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
 	}
 	else
 	{
